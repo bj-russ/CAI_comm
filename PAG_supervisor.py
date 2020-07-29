e7121ec0b4c727a6b62794a83970f7093fc03c5c
tmacinc <tmack090@gmail.com>
2020-07-29 12:07:32 -0400

    Update

--- PAG_supervisor.py
+++ PAG_supervisor.py
@@ -2,12 +2,30 @@
 
 import datetime
 import AK_handler
+#import AK_commands as com
 import socket
 import pandas as pd
+import os.path
+from os import path
 
 #load settings from config.cfg
-config = pd.read_csv('config.csv')
-HOST = config.host 
-PORT = config.port 
+print(repr('/Users/Tom/Desktop/WFH/PAG Varification/PAG_varifier/config.csv'))
 
-print(HOST, PORT)
+config = pd.read_csv('/Users/Tom/Desktop/WFH/PAG Varification/PAG_varifier/config.csv')
+HOST = config.iloc[0,0]
+PORT = config.iloc[0,1] 
+#add additional settings here
+
+#define functions
+
+def log(status, reading):
+    time = datetime.datetime.now()
+    t = time.strftime("%Y %b %d %H:%M:%S")
+    results = [t, status, reading]
+    df = pd.DataFrame([results], columns=['Time of test', 'Test Result', 'Reading'])
+    if path.isfile('PAG_log.csv'):
+        df.to_csv('PAG_log.csv', mode = 'a', header=False)
+    else:
+        df.to_csv('PAG_log.csv', mode = 'a', header=True)
+
+log('Pass', 0)
